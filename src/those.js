585461ea791ea9d668e47db2c8ac081f5bcd0015
geoffreyfloyd <geoffrey.floyd@hoomanlogic.com>
2016-01-01 00:55:47 -0700

    add more funcs

--- src/those.js
+++ src/those.js
@@ -14,8 +14,12 @@
         array.top = top;
         array.order = order;
         array.like = like;
+		array.notLike = notLike;
         array.first = first;
+		array.flip = flip;
+		array.flick = flick;
         array.pluck = pluck;
+		array.toggle = toggle;
         
         return array;
     };
@@ -94,13 +98,51 @@
         return Object.prototype.toString.call(arg) === '[object Object]';
     };
 
+	var notLike = function (matchObj) {
+        var matches = [];
+        for (var i = 0; i < this.length; i++) {
+            // We assume it matches until we prove it doesn't
+            var isLike = true;
+            if (typeof matchObj === 'string') {
+                if (this[i] !== matchObj) {
+                    isLike = false;
+                }
+            }
+            else {
+                for (var matchProp in matchObj) {
+                    if (matchObj.hasOwnProperty(matchProp)) {
+                        var t = typeof matchObj[matchProp];
+                        if (t !== typeof this[i][matchProp]) {
+                            isLike = false;
+                            break;
+                        }
+                        if (isArray(matchObj[matchProp]) && !those(matchObj[matchProp]).hasOnly(this[i][matchProp])) {
+                            isLike = false;
+                            break;
+                        }
+                        else if (String(matchObj[matchProp]) !== String(this[i][matchProp])) {
+                            isLike = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            // If not all the match props matched, then add to result
+            if (!isLike) {
+                matches.push(this[i]);
+            }
+        }
+        // Return new array of matched items
+        return extend(matches);
+    };
+	
     var like = function (matchObj) {
         var matches = [];
         for (var i = 0; i < this.length; i++) {
             // We assume it matches until we prove it doesn't
             var isLike = true;
             if (typeof matchObj === 'string') {
-                if (this[i] === matchObj) {
+                if (this[i] !== matchObj) {
                     isLike = false;
                 }
             }
@@ -141,15 +183,44 @@
     };
 
     var order = function (prop) {
-        return extend(this.sort(function (a, b) {
-            if (prop) {
-                return a[prop] > b[prop] ? 1 : (a[prop] < b[prop] ? -1 : 0);
+		
+        this.sort(function (a, b) {
+			var am, bm;
+			
+            if (typeof prop === 'func') {
+				am = prop(a);
+				bm = prop(b);
+			} 
+			else if (prop) {
+				if (typeof a[prop] === 'string' && typeof b[prop] === 'string') {
+					am = a[prop].toLowerCase();
+					bm = b[prop].toLowerCase();
+				}
+				else {
+					am = a[prop];
+					bm = b[prop];
+				}
             }
             else {
-                return a > b ? 1 : (a < b ? -1 : 0);
+				if (typeof a === 'string' && typeof b === 'string') {
+					am = a.toLowerCase();
+					bm = b.toLowerCase();
+				}
+				else {
+					am = a;
+					bm = b;
+				}
             }
-        }));
+			
+			return am > bm ? 1 : (am < bm ? -1 : 0);
+        });
+		return this;
     };
+	
+	var flip = function () {
+		this.reverse();
+		return this;
+	};
 
     var first = function (matchObj) {
 		// top 1
@@ -166,7 +237,7 @@
             // We assume it matches until we prove it doesn't
             var isLike = true;
             if (typeof matchObj === 'string') {
-                if (this[i] === matchObj) {
+                if (this[i] !== matchObj) {
                     isLike = false;
                 }
             }
@@ -196,6 +267,62 @@
         }
         return null;
     };
+	
+	var toggle = function (obj) {
+		if (this.first(obj)) {
+			this.flick(obj);
+		}
+		else {
+			this.push(obj);
+		}
+	};
+	
+    var flick = function (matchObj) {
+		// top 1
+		if (matchObj === undefined) {
+			if (this.length === 0) {
+				return extend([]);
+			}
+			else {
+				return extend(this.slice(1));
+			}
+		}
+		
+        for (var i = 0; i < this.length; i++) {
+            // We assume it matches until we prove it doesn't
+            var isLike = true;
+            if (typeof matchObj === 'string') {
+                if (this[i] !== matchObj) {
+                    isLike = false;
+                }
+            }
+            else {
+                for (var matchProp in matchObj) {
+                    if (matchObj.hasOwnProperty(matchProp)) {
+                        var t = typeof matchObj[matchProp];
+                        if (t !== typeof this[i][matchProp]) {
+                            isLike = false;
+                            break;
+                        }
+                        if (isArray(matchObj[matchProp]) && !those(matchObj[matchProp]).hasOnly(this[i][matchProp])) {
+                            isLike = false;
+                            break;
+                        }
+                        else if (String(matchObj[matchProp]) !== String(this[i][matchProp])) {
+                            isLike = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            // If all match props matched, then add to result
+            if (isLike) {
+                this.splice(i, 1);
+				return this;
+            }
+        }
+        return this;
+    };
 
     var pluck = function (prop) {
         var plucked = [];
@@ -207,7 +334,7 @@
 
     var those = function (array) {
         return extend(array);
-    }
+    };
     
     return those;
 }));
